d0f2bcde0893f5dc204cc0aeba18721743b08d86
ivanh96 <ivanh96@hotmail.com>
2016-07-12 14:04:18 -0600

    Update Principal.java

--- Radio/src/Principal.java
+++ Radio/src/Principal.java
@@ -5,20 +5,8 @@ import java.util.*;
  * 
  */
 public class Principal {
-
-    /**
-     * 
-     */
-    public Principal() {
-    }
-
-    /**
-     * @param String[]args 
-     * @return
-     */
-    public void main(void String[]args) {
-        // TODO implement here
-        return null;
+    public static void main(String[] args) {
+       
     }
 
 }

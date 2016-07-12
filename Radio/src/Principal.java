9e4da3aa1ebbbc1de8eb2c9174a70b7176f82b77
ivanh96 <ivanh96@hotmail.com>
2016-07-12 14:47:12 -0600

    Update Principal.java

--- Radio/src/Principal.java
+++ Radio/src/Principal.java
@@ -1,12 +1,39 @@
 
 import java.util.*;
+import java.util.Scanner;
 
 /**
  * 
  */
 public class Principal {
     public static void main(String[] args) {
-       
+
+    	Scanner input = new Scanner(System.in);
+    	Carro carro = new Carro();
+
+
+    	System.out.println("+--------------------------------------------+");
+        System.out.println("|   estado: "+carro.estado+"                            |");
+        System.out.println("|   frecuencia: "+carro.frecuencia+"                        |");        
+        System.out.println("|   emisora: "+carro.emisora+"                             |");
+        System.out.println("|                                            |");
+        System.out.println("|                                            |");
+        System.out.println("|  (1)(2))(3)(4)(5)(6)(7)(8)(9)(10)(11)(12)  |");
+        System.out.println("+--------------------------------------------+");
+        System.out.println("");
+    	System.out.println("+--------------------------------------------+");
+        System.out.println("|                                            |");
+    	System.out.println("+--------------------------------------------+");
+        System.out.println("");
+    	System.out.println("+--------------------------------------------+");
+        System.out.println("|                                            |");
+        System.out.println("|                                            |");
+        System.out.println("|                                            |");
+        System.out.println("|                                            |");
+        System.out.println("|                                            |");
+    	System.out.println("+--------------------------------------------+");
+        System.out.println("");
+
     }
 
 }
